a4633c0b9fcaa99319cb4f62ccf0edb0cff74960
Moises A <a.aguilar.moises@gmail.com>
2020-04-18 04:45:21 +0100

    Look 2 commits in one day

--- games/models.py
+++ games/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+import os
 # Create your models here.
 
 #wondering if it would be wise to allow for uploading a js file
@@ -9,7 +10,7 @@ class Game(models.Model):
 	text = models.TextField(max_length=500, blank=True)
 	path = models.SlugField(max_length=50, unique=True)
 	creator = models.ForeignKey(User, on_delete=models.CASCADE)
-	js = models.FilePathField(path='static/games', recursive=True, blank=True, match=".*js")
+	js = models.FilePathField(path=os.path.join(settings.STATIC_ROOT,'games'), recursive=True, blank=True, match=".*js")
 
 	def __str__(self):
 		return self.name
